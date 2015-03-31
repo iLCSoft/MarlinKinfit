77fe2d8cfb64e4587524f00cf90fc534cccc3422
Jenny List <Jenny.List@desy.de>
2015-03-31 14:26:46 +0000

    oops, one more

--- src/BaseFitObject.cc
+++ src/BaseFitObject.cc
@@ -103,7 +103,7 @@ bool BaseFitObject::updateParams (double p[], int idim) {
   for (int ilocal = 0; ilocal < getNPar(); ++ilocal) {
     int iglobal = getGlobalParNum (ilocal);
     assert (iglobal >= 0 && iglobal < idim);
-    bool parameterChanged = setParam (ilocal, p[iglobal])
+    bool parameterChanged = setParam (ilocal, p[iglobal]);
     result = result || parameterChanged;
     // if illegal value: read back legal value
     p[iglobal] = getParam (ilocal);
